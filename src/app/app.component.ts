c2061e65ce4941635609c76def765473f111be67
unknown <ayoubaouinti1990@gmail.com>
2024-09-06 17:33:57 +0200

    create navbar & footer & toast fonctionnalitys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,51 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonModule} from "primeng/button";
-import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { ButtonModule } from "primeng/button";
+import { FaIconLibrary, FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
+import { ToastModule } from "primeng/toast";
+import { ToastService } from './layout/toast.service';
+import { MessageService } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [
+    RouterOutlet, 
+    ButtonModule, 
+    FontAwesomeModule, 
+    NavbarComponent, 
+    FooterComponent, 
+    ToastModule
+  ],
+  providers: [MessageService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']  // Correct the typo: 'styleUrl' to 'styleUrls'
 })
 export class AppComponent implements OnInit {
-  faIconLibrary: FaIconLibrary = inject (FaIconLibrary);
+  faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+  isListingView = true;
+  toastService = inject(ToastService);
+  messageService = inject(MessageService);
 
   ngOnInit(): void {
-      this.initFontAwesome();
+    this.initFontAwesome();
+    this.listenToastService();
   }
 
-  private initFontAwesome(): void{
+  private initFontAwesome(): void {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  private listenToastService(): void {
+    this.toastService.sendSub.subscribe({
+      next: (newMessage) => {  // Explicitly type the 'newMessage' parameter
+        if (newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+          this.messageService.add(newMessage);
+        }
+      }
+    });
   }
 }
--- src/app/layout/toast.service.ts
+++ src/app/layout/toast.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Message } from 'primeng/api'; // Correct import of Message type
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
+  // Define the initial state as a constant
+  INIT_STATE: string = 'INIT';
 
-  constructor() { }
+  // Private subject to handle the messages
+  private send$ = new BehaviorSubject<Message>({ summary: this.INIT_STATE });
+  
+  // Public observable that components can subscribe to
+  sendSub = this.send$.asObservable();
+
+  // Method to send a new message
+  public send(message: Message): void {
+    this.send$.next(message);
+  }
 }
